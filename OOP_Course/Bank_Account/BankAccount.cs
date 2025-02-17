5c2effcf21c98b67b1e165054d23a0a0ee37bc1a
samir-fawzy <sfawzy862@gamil.com>
2025-02-17 19:26:11 +0200

    Add main files

---
+++ OOP_Course/Bank_Account/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOPCourse.Bank_Account
+{
+    internal class BankAccount
+    {
+
+        // Constructor
+        public BankAccount(string name,int age,string address,int accountNumber,string phone)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+            AccountNumber = accountNumber;
+            Phone = phone;
+        }
+
+        // Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public int AccountNumber { get; set; }
+        public string Phone { get; set; }
+
+        // Method to display customer account data
+        public void DisplayAccountData()
+        {
+            Console.WriteLine("******************** Display Account Information ********************");
+            Console.WriteLine($"Name           : {Name}");
+            Console.WriteLine($"Age            : {Age}");
+            Console.WriteLine($"Address        : {Address}");
+            Console.WriteLine($"Account Number : {AccountNumber}");
+            Console.WriteLine($"Phone          : {Phone}");
+        }
+
+        // Method to modify data account
+        public void ModifyData(string name, int age, string address, int accountNumber, string phone)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+            AccountNumber = accountNumber;
+            Phone = phone;
+            Console.WriteLine("Data Modified Successfully");
+        }
+    }
+}
---
+++ OOP_Course/Exeption_Handling/Exception_Handling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOPCourse.Exeption_Handle
+{
+    internal class Exception_Handling
+    {
+        public void PrintDivid(int num1,int num2)
+        {
+            try
+            {
+                int result = num1 / num2;
+                Console.WriteLine($"Result : {result}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Arr()
+        {
+            int[] arr = {1,2,3};
+            try
+            {
+                Console.WriteLine(arr[10]);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
