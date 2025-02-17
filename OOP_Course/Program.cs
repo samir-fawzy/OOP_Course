19ec56ecbcdd225c0d43e54971bebc9a8571ae57
samir-fawzy <sfawzy862@gamil.com>
2025-02-17 19:35:50 +0200

    complete Eception_Handling file

--- OOP_Course/Program.cs
+++ OOP_Course/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace OOP_Course
+namespace OOPCourse
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Bank Account
+            /*
+            Bank_Account.BankAccount customer1 = new Bank_Account.BankAccount("Samir Fawzy",21,"Shebin",1,"0123456789");
+
+            customer1.DisplayAccountData();
+            Console.WriteLine("***************************************");
+
+            customer1.ModifyData("ahmed fawzy", 31, "Cairo", 2, "24879287");
+            Console.WriteLine("***************************************");
+
+            customer1.DisplayAccountData();
+            Console.WriteLine("End Programe");
+            */
+
+            // Exeption Handling
+            Exeption_Handle.Exception_Handling eh = new Exeption_Handle.Exception_Handling();
+            //eh.PrintDivid(10, 0);
+            eh.Arr();
+
+            Console.ReadKey();
         }
     }
 }
